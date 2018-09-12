6e660af338dcc318d7798a028ccff9c83bfb3bb3
loveCGyang <41772779+loveCGyang@users.noreply.github.com>
2018-09-12 21:25:54 +0800

    Add files via upload

--- script/blogjs.js
+++ script/blogjs.js
@@ -79,6 +79,8 @@ function closeSwiper() {
     main.removeChild(swipers[0]);
 }
 showPartMsg();
+
+var ggg=1;
 function showPartMsg(index) {
     var i = 0;
     if(!index){
@@ -91,10 +93,19 @@ function showPartMsg(index) {
         var msg = msgs[i];
         msg.style.display = 'none';
     }
-    list.style.display = 'block';
-    msgs[index-1].style.display = 'block';
-    content.style.display = 'none';
+    if(index){
+        list.style.display = 'block';
+        msgs[index-1].style.display = 'block';
+        content.style.display = 'none';
+    }else{
+        list.style.display = 'block';
+        msgs[ggg-1].style.display = 'block';
+        content.style.display = 'none';
+    }
 }
+
+var back=getId("back111");
+
 
 function article(option) {
     option = option || {};
@@ -108,147 +119,152 @@ function article(option) {
     this.encrypt = option.encrypt || false;
 }
 
-var msg_array = [];
-var msg1,msg2,msg3,msg4,msg5,msg6,msg7,msg8,msg9,msg10,msg11,msg12,msg13,msg14;
+
+var msg1,msg2,msg3,msg4,msg5,msg6;
+
+
+var msg_array2 = [];//算法用的
 
 
 
-msg14 = new article({
+
+msg_array2.push(new article({
     caption:'关于POJ1222熄灯问题',
     type:'算法',
     time:'2018/9/05',
-    index:15,
-    encrypt:false,
-    content:''
-});
-msg_array.push(msg14);
-
-msg_array.push(new article({
-    caption:'脱壳笔记',
-    type:'LCG文章',
-    time:'2018/8/25',
-    index:14,
+    index:8,
     encrypt:false,
     content:''
 }));
 
-msg13 = new article({
-    caption:'80款大型精品游戏百度网盘合集',
-    type:'福利分享',
-    time:'2018/7/20',
-    index:13,
-    encrypt:true,
-    content:''
-});
-msg_array.push(msg13);
-
-msg12 = new article({
-    caption:'TraceMe暴力破解法与逆向分析法',
-    type:'LCG文章',
-    time:'2018/7/15',
-    index:12,
-    content:''
-});
-msg_array.push(msg12);
-
-msg11 = new article({
-    caption:'汇编学习大总结',
-    type:'汇编ASM',
-    time:'2018/7/10',
-    index:11,
-    content:''
-});
-msg_array.push(msg11);
-
-msg10 = new article({
-    caption:'GFW(Great Fire Wall)的认识',
-    type:'认知',
-    time:'2018/6/29',
-    index:10,
-    content:''
-});
-msg_array.push(msg10);
-
-msg9 = new article({
-    caption:'Python学习网盘链接',
-    type:'分享',
-    time:'2018/6/18',
-    index:9,
-    encrypt:true,
-    content:''
-});
-msg_array.push(msg9);
-
-msg8 = new article({
-    caption:'javascript判断用户是手机端还是PC端',
-    type:'JavaScript',
-    time:'2018/6/08',
-    index:8,
-    content:''
-});
-msg_array.push(msg8);
-
-msg7 = new article({
+msg_array2.push(new article({
     caption:'The theory of creating array',
     type:'Data Structure',
     time:'2018/5/24',
     index:7,
     content:''
-});
-msg_array.push(msg7);
+}));
 
-msg6 = new article({
+msg_array2.push(new article({
     caption:'Traverse Graph(DFS and BFS)',
     type:'Data Structure',
     time:'2018/5/18',
     index:6,
     content:''
+}));
+
+
+msg_array2.push(new article({
+    caption:'Three ways of building Graph',
+    type:'Data Structure',
+    time:'2018/5/18',
+    index:5,
+    content:''
+}));
+
+
+msg_array2.push(new article({
+    caption:'Hash List',
+    type:'Data Structure',
+    time:'2018/4/29',
+    index:4,
+    content:''
+}));
+
+
+msg_array2.push(new article({
+    caption:'Insertion and deletion of binary tree nodes',
+    type:'Data Structure',
+    time:'2018/4/23',
+    index:3,
+    content:''
+}));
+
+
+msg_array2.push(new article({
+    caption:'Binary tree traversal and its image representation',
+    type:'Data Structure',
+    time:'2018/4/11',
+    index:2,
+}));
+
+msg_array2.push(new article({
+    caption:'A variety of sorting algorithm code details',
+    type:'Data Structure',
+    time:'2018/3/23',
+    index:1,
+}));
+
+var msg_array = [];
+msg_array.push(new article({
+    caption:'脱壳笔记',
+    type:'LCG文章',
+    time:'2018/8/25',
+    index:7,
+    encrypt:false,
+    content:''
+}));
+
+msg6 = new article({
+    caption:'80款大型精品游戏百度网盘合集',
+    type:'福利分享',
+    time:'2018/7/20',
+    index:6,
+    encrypt:true,
+    content:''
 });
 msg_array.push(msg6);
 
 msg5 = new article({
-    caption:'Three ways of building Graph',
-    type:'Data Structure',
-    time:'2018/5/18',
+    caption:'TraceMe暴力破解法与逆向分析法',
+    type:'LCG文章',
+    time:'2018/7/15',
     index:5,
     content:''
 });
 msg_array.push(msg5);
 
 msg4 = new article({
-    caption:'Hash List',
-    type:'Data Structure',
-    time:'2018/4/29',
+    caption:'汇编学习大总结',
+    type:'汇编ASM',
+    time:'2018/7/10',
     index:4,
     content:''
 });
 msg_array.push(msg4);
 
 msg3 = new article({
-    caption:'Insertion and deletion of binary tree nodes',
-    type:'Data Structure',
-    time:'2018/4/23',
+    caption:'GFW(Great Fire Wall)的认识',
+    type:'认知',
+    time:'2018/6/29',
     index:3,
     content:''
 });
 msg_array.push(msg3);
 
 msg2 = new article({
-    caption:'Binary tree traversal and its image representation',
-    type:'Data Structure',
-    time:'2018/4/11',
+    caption:'Python学习网盘链接',
+    type:'分享',
+    time:'2018/6/18',
     index:2,
+    encrypt:true,
+    content:''
 });
 msg_array.push(msg2);
 
 msg1 = new article({
-    caption:'A variety of sorting algorithm code details',
-    type:'Data Structure',
-    time:'2018/3/23',
+    caption:'javascript判断用户是手机端还是PC端',
+    type:'JavaScript',
+    time:'2018/6/08',
     index:1,
+    content:''
 });
-
 msg_array.push(msg1);
+
+
+
+
+
 
 var model_lock = getId('model_lock');
 /**首先给文章列表添加p标签*/
@@ -261,14 +277,32 @@ for (; i<len ;i++){
     }else {
         a= '';
     }
+
     msgAll.innerHTML += '<p encrypt="'+p.encrypt+'" onclick="showMessage('+p.index+')">'+(i+1)+'. ['+ p.type+'] '+ p.caption+a+ '<span class="w3-right">'+ p.time+'</span></p>';
+
 }
+
+var msgAll2 =getId('allMsg2');
+i=0;len = msg_array2.length;
+for (; i<len ;i++){
+    var p = msg_array2[i];
+    if(p.encrypt){
+        var a= '  '+lock;
+    }else {
+        a= '';
+    }
+
+    msgAll2.innerHTML += '<p encrypt="'+p.encrypt+'" onclick="showMessage('+p.index+',0)">'+(i+1)+'. ['+ p.type+'] '+ p.caption+a+ '<span class="w3-right">'+ p.time+'</span></p>';
+
+}
+
 var caption = getId('msg_caption');
 var author = getId('msg_author');
 var time = getId('msg_time');
 
 var syntaxPart = getId('syntax');
-function showMessage(index) {
+var syntaxPart2 = getId('syntax2');
+function showMessage(index,ii=1) {
     // console.log(msg_array[msg_array.length-index]);
     // var b =msg_array[msg_array.length-index];
     // console.log(b);
@@ -276,26 +310,50 @@ function showMessage(index) {
     //     model_lock.style.display = 'block';
     //     return ;
     // }设置加密，暂时先不做  2018.8.4
-    content.style.display = 'block';
-    list.style.display = 'none';
-    var op = msg_array[msg_array.length-index];
-    caption.innerText = op.caption;
-    author.innerText = op.author;
-    time.innerText = op.time;
-    showPre(msg_array.length-index);
+    if(ii === 1){
+        ggg=1;
+        content.style.display = 'block';
+        list.style.display = 'none';
+        var op = msg_array[msg_array.length-index];
+        caption.innerText = op.caption;
+        author.innerText = op.author;
+        time.innerText = op.time;
+        showPre(msg_array.length-index);
+    }else {
+        ggg=2;
+        content.style.display = 'block';
+        list.style.display = 'none';
+        var op = msg_array2[msg_array2.length-index];
+        caption.innerText = op.caption;
+        author.innerText = op.author;
+        time.innerText = op.time;
+        showPre(msg_array2.length-index,0);
+    }
 
 }
 function closeModel() {
     model_lock.style.display = 'none';
 }
 
-function showPre(index) {
-   var i=0,len = syntaxPart.children.length;
+function showPre(index,ii=1) {
+    var i=0,len = syntaxPart.children.length;
     for (;i< len ; i++){
         syntaxPart.children[i].style.display='none';
     }
-    syntaxPart.children[index].style.display='block';
-    syntaxPart.children[index].style.marginTop='20px';
+    i=0;len = syntaxPart2.children.length;
+    for (;i< len ; i++){
+        syntaxPart2.children[i].style.display='none';
+    }
+
+   if(ii){
+
+       syntaxPart.children[index].style.display='block';
+       syntaxPart.children[index].style.marginTop='20px';
+   }else{
+
+       syntaxPart2.children[index].style.display='block';
+       syntaxPart2.children[index].style.marginTop='20px';
+   }
 }
 
 function getInterval(start,end) {
